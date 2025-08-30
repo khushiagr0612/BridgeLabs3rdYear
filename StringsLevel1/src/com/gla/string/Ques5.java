cd83c8d24af20dbb5b5228fd2a4077d68e95473c
Khushi Agrawal <158738706+khushiagr0612@users.noreply.github.com>
2025-08-30 23:53:03 +0530

    Add files via upload

---
+++ StringsLevel1/src/com/gla/string/Ques5.java
@@ -0,0 +1,26 @@
+package com.gla.string;
+import java.util.Scanner;
+
+public class Ques5 {
+
+    public static void generateException(String text) {
+        System.out.println(text.charAt(text.length()));
+    }
+
+    public static void handleException(String text) {
+        try {
+            System.out.println(text.charAt(text.length()));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Handled Exception: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter text: ");
+            String text = sc.next();
+            // generateException(text);
+            handleException(text);
+        }
+    }
+}
