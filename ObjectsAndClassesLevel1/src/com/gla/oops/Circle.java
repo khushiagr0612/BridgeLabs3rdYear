88541d4d3b488dee729667842bc8bc5f87432996
Khushi Agrawal <158738706+khushiagr0612@users.noreply.github.com>
2025-09-10 21:33:04 +0530

    Add files via upload

---
+++ ObjectsAndClassesLevel1/src/com/gla/oops/Circle.java
@@ -0,0 +1,24 @@
+package com.gla.oops;
+
+public class Circle {
+    double radius;
+
+    // constructor
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public void areaCircle() {
+        System.out.printf("Area of circle: %.4f", (Math.PI * radius * radius));
+    }
+
+    public void circumferenceCircle() {
+        System.out.printf("\nCircumference of circle: %.4f ", (2 * Math.PI * radius));
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.5);
+        circle.areaCircle();
+        circle.circumferenceCircle();
+    }
+}
